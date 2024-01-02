6c907991e62128e577181ed710f95943a0073d21
Juan A Mangubat <jman0023@student.monash.edu>
2024-01-03 00:09:24 +1100

    baseURL change

--- src/components/API/ApiClient.js
+++ src/components/API/ApiClient.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export const apiClient = axios.create(
      {
           baseURL: 'http://localhost:8080'
+          // baseURL: Watch-list-rest-api-env.eba-xdkb2amp.ap-southeast-2.elasticbeanstalk.com'
      }
 )
