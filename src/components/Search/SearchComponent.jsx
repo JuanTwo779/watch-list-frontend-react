d081fda51a11b838603f6dce78b42e65d21f950d
juanm <juanmangubat@gmail.com>
2024-02-28 22:29:44 +1100

    search filter styling

--- src/components/Search/SearchComponent.jsx
+++ src/components/Search/SearchComponent.jsx
@@ -32,54 +32,73 @@ export default function SearchComponent(){
      }
      
      return (
-          <div>
-            <h1>Movie Search</h1>
-            <input
-              type="text"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Enter movie title"
-            />
-            <input
-              type="text"
-              value={language}
-              onChange={(e) => setLanguage(e.target.value)}
-              placeholder="Language"
-            />
-            <input
-              type="text"
-              value={primaryReleaseYear}
-              onChange={(e) => setPrimaryReleaseYear(e.target.value)}
-              placeholder="Primary Release Year"
-            />
-            <input
-              type="number"
-              value={page}
-              onChange={(e) => setPage(parseInt(e.target.value))}
-              placeholder="Page"
-            />
-            <input
-              type="text"
-              value={region}
-              onChange={(e) => setRegion(e.target.value)}
-              placeholder="Region"
-            />
-            <input
-              type="text"
-              value={year}
-              onChange={(e) => setYear(e.target.value)}
-              placeholder="Year"
-            />
-            <button onClick={handleSearch}>Search</button>
-
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-              {movies.map((movie, id) =>{
-                return <SearchCard key={id} {...movie} />
-              })}
-              
-            </div>
-
-
+        <div>
+          <h1>Movie Search</h1>
+          <div className="filter-container">
+              <div className="filter">
+                  <p className="filter-title">Keywords</p>
+                  <input
+                      type="text"
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                      placeholder="E.g. godfather, part"
+                  />
+              </div>
+              <div className="filter">
+                  <p className="filter-title">Language</p>
+                  <input
+                      type="text"
+                      value={language}
+                      onChange={(e) => setLanguage(e.target.value)}
+                      placeholder=""
+                  />
+              </div>
+              <div className="filter">
+                  <p className="filter-title">Release Year</p>
+                  <input
+                      type="text"
+                      value={primaryReleaseYear}
+                      onChange={(e) => setPrimaryReleaseYear(e.target.value)}
+                      placeholder="2000"
+                  />
+              </div>
+              <div className="filter">
+                  <p className="filter-title">Page</p>
+                  <input
+                      type="number"
+                      value={page}
+                      onChange={(e) => setPage(parseInt(e.target.value))}
+                      placeholder="Page"
+                  />
+              </div>
+              <div className="filter">
+                  <p className="filter-title">Country</p>
+                  <input
+                      type="text"
+                      value={region}
+                      onChange={(e) => setRegion(e.target.value)}
+                      placeholder="country code"
+                  />
+              </div>
+              <div className="filter">
+                  <p className="filter-title">Year</p>
+                  <input
+                      type="text"
+                      value={year}
+                      onChange={(e) => setYear(e.target.value)}
+                      placeholder="####"
+                  />
+              </div>
           </div>
+          <button onClick={handleSearch}>Search</button>
+      
+          <div className="search-results rounded">
+              {movies.map((movie, id) => {
+                  return <SearchCard key={id} {...movie} />
+              })}
+      
+          </div>
+      </div>
+  
         );
 }
