9fffa9f02bfb601dddb03116d81f6425341130c3
juanm <juanmangubat@gmail.com>
2024-08-21 23:53:04 +1000

    onClick buttons on searchCard

--- src/components/Search/SearchCard.jsx
+++ src/components/Search/SearchCard.jsx
@@ -3,22 +3,22 @@ const getPostUrl = (posterPath) =>{
      return `https://media.themoviedb.org/t/p/w220_and_h330_face/${posterPath}`
 }
 
-const SearchCard = ({id, poster_path, title, overview, release_date, onDetailsClick, onAddClick}) =>{
+const SearchCard = ({poster_path, title, overview, release_date, onDetailsClick, onAddClick}) =>{
 
      return(
-          <div class="card p-2 m-2">
-               <img class="card-img-top" src={getPostUrl(poster_path)} alt={title}/>
-               <div class="card-body">
-                    <h5 class="card-title">{title}</h5>
-                    <p class="card-text">{overview}</p>
+          <div className="card p-2 m-2">
+               <img className="card-img-top" src={getPostUrl(poster_path)} alt={title}/>
+               <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{overview}</p>
                </div>
-               <ul class="list-group list-group-flush">
-                    <li class="list-group-item">{release_date}</li>
+               <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{release_date}</li>
                </ul>
                <div className="card-body">
-                <button className="btn btn-outline-primary m-1" onClick={() => onDetailsClick(id)}>Details</button>
-                <button className="btn btn-outline-success m-1" onClick={() => onAddClick(id)}>Add</button>
-            </div>
+                <button className="btn btn-outline-primary m-1" onClick={() => onDetailsClick()}>Details</button>
+                <button className="btn btn-outline-success m-1" onClick={() => onAddClick()}>Add</button>
+               </div>
           </div>
      )
 }
